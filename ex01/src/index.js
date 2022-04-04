6757c5676084dd56d33fe6c598342154ec7b3c9a
Haris Pasic <harispasic@ymail.com>
2022-04-05 01:13:54 +0200

    Functions

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,9 @@
+function reusableFunction() {
+    //write code below this line
+    console.log("Hi Padawan!");
+    //write code above this line
+}
 
+console.log(reusableFunction());
+
+module.exports = reusableFunction;
