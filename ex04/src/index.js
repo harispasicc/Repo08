8f998765c4b4bdfd619fa17b215921897853923a
Haris Pasic <harispasic@ymail.com>
2022-04-05 01:38:51 +0200

    Functions - Global Scope

---
+++ ex04/src/index.js
@@ -0,0 +1,24 @@
+var myGlobalVariable = 10;
+
+function function1() {
+    myLocalVariable = 5;
+}
+
+function function2() {
+    var result = "";
+    if(typeof myGlobalVariable != "undefined") {
+        result += "myGlobalVariable: " + myGlobalVariable;
+    }
+    if (typeof myLocalVariable != "undefined") {
+        result += " myLocalVariable: " + myLocalVariable;
+    }
+    console.group(result);
+}
+
+function1();
+function2();
+
+module.exports = {
+    function1,
+    function2
+};
