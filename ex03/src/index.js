5ae4d1f3b120521cbea23725defe3f284a82b57a
Haris Pasic <harispasic@ymail.com>
2022-04-05 05:01:48 +0200

    Functions - Local Scope

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -5,9 +5,9 @@ function localScope() {
 localScope();
 
 if (typeof myVariable != "undefined") {
-    console.log('outside localScope', myVariable)
+    console.log('outside localScope', myVariable);
 } else {
-    console.log('outside localScope UNDEFINED!!!')
+    console.log('outside localScope UNDEFINED!!!');
 }
 
 module.exports = localScope;
