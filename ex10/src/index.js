9d16dbbf5a845322ff4de7a28ef9f597dadbccf4
Haris Pasic <harispasic@ymail.com>
2022-04-05 05:04:08 +0200

    parseInt()

---
+++ ex10/src/index.js
@@ -0,0 +1,9 @@
+function convertStrToInt(str){
+return parseInt(str);
+}
+convertStrToInt()
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("27"));
+console.log(convertStrToInt("JeffBezzos"));
+
+module.exports = convertStrToInt;
